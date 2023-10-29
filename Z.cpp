744dea065f6e8a3bab1139581641cd2267e517dc
Elmer Williams <145784395+ElmWill@users.noreply.github.com>
2023-10-29 23:39:34 +0700

    Update Z.cpp

--- Z.cpp
+++ Z.cpp
@@ -1,44 +1,69 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int breath, stairs;
+    char step[10001];
+    int count1, countLast, countFirst, flag;
+    int t;
+
+    scanf("%d", &t);
+
+    for(int i = 1; i <= t; i++){
+        count1 = 0;
+        int value = 0;
+		countLast = 0;
+		countFirst = 0;
+		
+        scanf("%d %d", &stairs, &breath);
+        getchar(); 
+
+        scanf(" %[^\n]", step); 
 	
-	int t;
-	long long int n, breath, toxic[10001], temp;
-	char string[10001];
-	
-	scanf("%d", &t);
-	for(int i = 1; i <=t; i++){
-		scanf("%lld %lld", &n, &breath);
-		getchar();
-		for(long long int j = 1; j <= n; j++){
-			scanf("%c", &toxic[j]);
-		}
-		getchar();
-		temp = breath - 1;
-		for(long long int j = 1; j <= n; j++){
-			if(temp <= 0){
-				break;
+        for(int j = 0; j < stairs; j++ ){
+        	if(step[j] == '1'){
+			
+        	count1++;
 			}
-			if(toxic[j] == 0){
-				temp -= 2;
-				if(temp <= 0){
-					break;
+		}
+        if(count1 > 1){
+			for(int j = stairs - 1; j >= 0; j--){
+        		if(step[j] == '1'){
+        			break;
 				}
+				countLast++;
+			}    	
+        	for(int j = 0; j < stairs; j++){
+        		if(step[j] == '1'){
+        			break;
+				}
+				countFirst++;
 			}
-			else if(toxic[j] == 1){
-				toxic[j] = 0;
-				temp = breath - 1;
-			}
-		}
-		if(temp <= 0){
-//			printf("%lld\n", temp);
-			printf("Case #%d: Dead\n", i);
+        	
 		}
 		else{
-//			printf("%lld\n", temp);
-			printf("Case #%d: Alive\n", i);
+			if(breath >= stairs){
+				value = 0;				
+			}
+			if(breath < stairs){
+				value = 1;
+			}	
 		}
-	}
+		if(countFirst < breath && countLast < breath && count1 > 1){
+			value = 0;
+		}
+		else if(count1 > 1){
+			value = 1;
+		}
 	
-	return 0;
+        printf("Case #%d: ", i);
+        if(value == 0){
+            printf("Alive\n");
+        }
+		else{
+            printf("Dead\n");
+        }
+	}
+    
+
+    return 0;
 }
